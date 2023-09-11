701b3db64dd2b25ad2a6b3da76583c90fe527146
Davian Morris <davianmorris35@gmail.com>
2023-09-11 01:42:01 -0700

    competed lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Access the newly created id and append it to the DOM
+        const newId = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        // Handle errors by appending the error message to the DOM
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+      });
+  }
+  
+  // Call the submitData function to send the POST request
+  submitData('John Doe', 'johndoe@example.com');
+  
